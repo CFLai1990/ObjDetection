8599b31e2a4f0e49821c090146001dbe3cef6ac2
CFLai1990 <music_lcf@163.com>
2019-02-09 02:55:13 +0800

    Testing Detectron on the server. --ChufanLai

--- apis/annotation/odmsk.py
+++ apis/annotation/odmsk.py
@@ -2,7 +2,6 @@ from flask_socketio import emit
 from .__settings__ import API, fileDir, fileType
 import base64
 import os
-import json
 
 class apiClass(API):
   def __init__(self, logger, socket, message, namespace, detector):
@@ -22,13 +21,11 @@ class apiClass(API):
     imgPath = self.saveImage(obj)
     self.logger.info('Image saved')
     result = self.objDetector.inferParameters(imgPath)
-    print(type(result))
-    result = json.dumps(result)
     self.logger.info('Image detection finished')
     return result
 
   def execute(self, obj):
     # save the text into file
     result = self.OD(obj)
-    self.socket.emit(self.message, result, namespace=self.namespace)
+    self.socket.emit(self.message, {'result': result}, namespace=self.namespace)
     self.logger.info('Result sent')

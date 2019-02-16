cdf85332a91b9a6d506182b671baf837d4d8e649
CFLai1990 <music_lcf@163.com>
2019-02-17 07:05:55 +0800

    Testing color detection. --ChufanLai

--- apis/apis.py
+++ apis/apis.py
@@ -19,13 +19,13 @@ ROUTE_DICT = {
 class APIs:
     """The router for all APIs"""
     def __init__(self, logger, socket):
+        # the APIs dictionary
+        # namespace: apis_instance
+        self.apis_dict = {}
         self.socket = socket
         self.logger = Logger('APIs', logger)
         self.route_by_namespace()
         self.logger.info('Server started')
-        # the APIs dictionary
-        # namespace: apis_instance
-        self.apis_dict = {}
 
     def route_by_namespace(self):
         """Route for each API namespace"""

526a6bd8247799a1963b4d8ff4bbe24bfb434486
CFLai1990 <music_lcf@163.com>
2019-02-16 15:52:06 +0800

    Testing server. --ChufanLai

--- app.py
+++ app.py
@@ -102,8 +102,12 @@ if not app.debug:
 # if __name__ == '__main__':
 #     socketio.run(app)
 
+VERSION = 'dev' 
+# VERSION = 'public'
+
 # Or specify port manually:
 if __name__ == '__main__':
     ODapis = APIs(app.logger, socketio)
-    port = int(os.environ.get('PORT', 2020))
+    portNum = 2021 if VERSION == 'dev' else 2020
+    port = int(os.environ.get('PORT', portNum))
     socketio.run(app, debug = False, host='0.0.0.0', port=port)
--- static/js/dev/index.js
+++ static/js/dev/index.js
@@ -4,16 +4,18 @@ import FSocket from './filesocket.js'
   'OD_Image': get the image with masks
   'OD_Mask': get the mask parameters
 */
-const MESSAGE = 'OD_Image'
-// const MESSAGE = 'OD_Mask'
+// const MESSAGE = 'OD_Image'
+const MESSAGE = 'OD_Mask'
 const MACHINE = 'dl'
+const VERSION = 'dev'
+// const VERSION = 'public'
 let $ = window.$
 
 $(document).ready(function () {
     // Socket.io demo
   let socket = new ClientIO({
     'address': MACHINE,
-    'port': 2020,
+    'port': VERSION === 'dev' ? 2021 : 2020,
     'namespace': 'api/annotation'
   })
   let fsocket = new FSocket(socket, MESSAGE)

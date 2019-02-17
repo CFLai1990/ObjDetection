313eb2efea9aa302f5ae2000866610edb6e64dd1
CFLai1990 <music_lcf@163.com>
2019-02-17 20:36:14 +0800

    Testing color detection. --ChufanLai

--- apis/annotation/libs/objattrs.py
+++ apis/annotation/libs/objattrs.py
@@ -94,11 +94,13 @@ class ObjAttrs:
                     mask_i = cv2.inRange(img, color['low'][i], color['high'][i])
                     mask = cv2.bitwise_or(mask, mask_i)
             if color['code'] != 0:
+                print('before')
                 masked_img = np.zeros(img.shape)
                 masked_img[:, :, 1] = mask
                 masked_img[:, :, 2] = mask
                 masked_img[:, :, 3] = mask
                 cv2.imwrite(OUTPUT_DIR + '/' + str(color['code']) + '.jpg', masked_img)
+                print('after')
             color_map = np.empty((height, width), dtype=np.uint8)
             color_map.fill(color['code'])
             color_map = cv2.bitwise_and(color_map, color_map, mask=mask)

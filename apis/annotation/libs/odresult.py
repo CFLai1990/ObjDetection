a937fb5337b0d199ee713ff2b202140fe7f3b3a5
CFLai1990 <music_lcf@163.com>
2019-02-17 21:41:21 +0800

    Testing color detection. --ChufanLai

--- apis/annotation/libs/odresult.py
+++ apis/annotation/libs/odresult.py
@@ -42,6 +42,11 @@ class ODResultGenerator:
     def pack(self):
         """Pack the final result"""
         print('Pack started')
+        print(self.class_name)
+        print(self.score)
+        print(self.bbox)
+        print(self.masks)
+        print(self.attrs.color)
         result = {
             'class': self.class_name,
             'score': self.score,

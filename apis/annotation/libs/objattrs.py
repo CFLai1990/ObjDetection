f70835730350725a407093e39efe4beb2e25dfc6
CFLai1990 <music_lcf@163.com>
2019-02-20 09:46:59 +0800

    Testing size and position detection. --ChufanLai

--- apis/annotation/libs/objattrs.py
+++ apis/annotation/libs/objattrs.py
@@ -139,7 +139,6 @@ class ObjAttrs:
             'x': [float('inf'), -1],
             'y': [float('inf'), -1]
         }
-        print('1')
         for contour in contour_list:
             contour_area = cv2.contourArea(contour)
             areas.append(contour_area)
@@ -155,7 +154,6 @@ class ObjAttrs:
                 ctr_range['y'][0] = left_y
             if right_y > ctr_range['y'][1]:
                 ctr_range['y'][1] = right_y
-        print('2')
         return areas, {
             'area': area,
             'x_range': ctr_range['x'],
@@ -186,13 +184,9 @@ class ObjAttrs:
     def get_mask(self, mask_img, contour_list):
         """Get the colors inside the mask"""
         # mask_img: the binary masked image
-        print('a')
         color = self.get_mask_color(mask_img)
-        print('b')
         areas, size = self.get_mask_size(contour_list)
-        print('c')
         position = self.get_mask_position(contour_list, areas, size['area'])
-        print('d')
         return {
             'color': color,
             'size': size,

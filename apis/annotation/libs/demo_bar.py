ecc1d28fe6fa8adca6463ddc3949d367819bff80
CFLai1990 <music_lcf@163.com>
2019-03-03 12:49:51 +0800

    Testing axes and legends for the demo image. --ChufanLai

---
+++ apis/annotation/libs/demo_bar.py
@@ -0,0 +1,774 @@
+"""The fake data for demo_bar.png"""
+
+# Data Entities
+BAR_DATA = [
+    {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "blue": 1.0
+        },
+        "bbox": {
+            "x": 178,
+            "y": 126,
+            "width": 54,
+            "height": 919
+            },
+        "mask": [[
+            [179, 127],
+            [179, 1044],
+            [231, 1044],
+            [231, 127]
+        ]],
+        "position": {
+            "x": 205,
+            "y": 585.5
+        },
+        "size": {
+            "area": 47684,
+            "x_range": [179, 231],
+            "y_range": [127, 1044]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "orange": 1.0
+        },
+        "bbox": {
+            "x": 233,
+            "y": 732,
+            "width": 54,
+            "height": 314
+            },
+        "mask": [[
+            [234, 733],
+            [234, 1045],
+            [286, 1045],
+            [286, 733]
+        ]],
+        "position": {
+            "x": 260,
+            "y": 889
+        },
+        "size": {
+            "area": 16224,
+            "x_range": [234, 286],
+            "y_range": [733, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "green": 1.0
+        },
+        "bbox": {
+            "x": 289,
+            "y": 934,
+            "width": 54,
+            "height": 112
+            },
+        "mask": [[
+            [290, 935],
+            [290, 1045],
+            [342, 1045],
+            [342, 935]
+        ]],
+        "position": {
+            "x": 316,
+            "y": 990
+        },
+        "size": {
+            "area": 5720,
+            "x_range": [290, 342],
+            "y_range": [935, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "blue": 1.0
+        },
+        "bbox": {
+            "x": 392,
+            "y": 234,
+            "width": 54,
+            "height": 812
+            },
+        "mask": [[
+            [393, 235],
+            [393, 1045],
+            [445, 1045],
+            [445, 235]
+        ]],
+        "position": {
+            "x": 419,
+            "y": 640
+        },
+        "size": {
+            "area": 42120,
+            "x_range": [393, 445],
+            "y_range": [235, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "orange": 1.0
+        },
+        "bbox": {
+            "x": 447,
+            "y": 649,
+            "width": 54,
+            "height": 397
+            },
+        "mask": [[
+            [448, 650],
+            [448, 1045],
+            [500, 1045],
+            [500, 650]
+        ]],
+        "position": {
+            "x": 474,
+            "y": 847.5
+        },
+        "size": {
+            "area": 20540,
+            "x_range": [448, 500],
+            "y_range": [650, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "green": 1.0
+        },
+        "bbox": {
+            "x": 503,
+            "y": 897,
+            "width": 54,
+            "height": 149
+            },
+        "mask": [[
+            [504, 898],
+            [504, 1045],
+            [556, 1045],
+            [556, 898]
+        ]],
+        "position": {
+            "x": 530,
+            "y": 971.5
+        },
+        "size": {
+            "area": 7644,
+            "x_range": [504, 556],
+            "y_range": [898, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "blue": 1.0
+        },
+        "bbox": {
+            "x": 605,
+            "y": 381,
+            "width": 54,
+            "height": 664
+            },
+        "mask": [[
+            [606, 382],
+            [606, 1044],
+            [658, 1044],
+            [658, 382]
+        ]],
+        "position": {
+            "x": 632,
+            "y": 713
+        },
+        "size": {
+            "area": 34424,
+            "x_range": [606, 658],
+            "y_range": [382, 1044]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "orange": 1.0
+        },
+        "bbox": {
+            "x": 661,
+            "y": 578,
+            "width": 54,
+            "height": 467
+            },
+        "mask": [[
+            [662, 579],
+            [662, 1044],
+            [714, 1044],
+            [714, 579]
+        ]],
+        "position": {
+            "x": 688,
+            "y": 811.5
+        },
+        "size": {
+            "area": 24180,
+            "x_range": [662, 714],
+            "y_range": [579, 1044]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "green": 1.0
+        },
+        "bbox": {
+            "x": 717,
+            "y": 828,
+            "width": 53,
+            "height": 217
+            },
+        "mask": [[
+            [718, 829],
+            [718, 1044],
+            [769, 1044],
+            [769, 829]
+        ]],
+        "position": {
+            "x": 743.5,
+            "y": 936.5
+        },
+        "size": {
+            "area": 10965,
+            "x_range": [718, 769],
+            "y_range": [829, 1044]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "blue": 1.0
+        },
+        "bbox": {
+            "x": 819,
+            "y": 411,
+            "width": 53,
+            "height": 634
+            },
+        "mask": [[
+            [820, 412],
+            [820, 1044],
+            [871, 1044],
+            [871, 412]
+        ]],
+        "position": {
+            "x": 845.5,
+            "y": 728
+        },
+        "size": {
+            "area": 32232,
+            "x_range": [820, 871],
+            "y_range": [412, 1044]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "orange": 1.0
+        },
+        "bbox": {
+            "x": 874,
+            "y": 597,
+            "width": 54,
+            "height": 449
+            },
+        "mask": [[
+            [875, 598],
+            [875, 1045],
+            [927, 1045],
+            [927, 598]
+        ]],
+        "position": {
+            "x": 901,
+            "y": 821.5
+        },
+        "size": {
+            "area": 23244,
+            "x_range": [875, 927],
+            "y_range": [598, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "green": 1.0
+        },
+        "bbox": {
+            "x": 930,
+            "y": 768,
+            "width": 54,
+            "height": 278
+            },
+        "mask": [[
+            [931, 769],
+            [931, 1045],
+            [983, 1045],
+            [983, 769]
+        ]],
+        "position": {
+            "x": 957,
+            "y": 907
+        },
+        "size": {
+            "area": 14352,
+            "x_range": [931, 983],
+            "y_range": [769, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "blue": 1.0
+        },
+        "bbox": {
+            "x": 1032,
+            "y": 379,
+            "width": 54,
+            "height": 667
+            },
+        "mask": [[
+            [1033, 380],
+            [1033, 1045],
+            [1085, 1045],
+            [1085, 380]
+        ]],
+        "position": {
+            "x": 1059,
+            "y": 712.5
+        },
+        "size": {
+            "area": 34580,
+            "x_range": [1033, 1085],
+            "y_range": [380, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "orange": 1.0
+        },
+        "bbox": {
+            "x": 1088,
+            "y": 600,
+            "width": 54,
+            "height": 446
+            },
+        "mask": [[
+            [1089, 601],
+            [1089, 1045],
+            [1141, 1045],
+            [1141, 601]
+        ]],
+        "position": {
+            "x": 1115,
+            "y": 823
+        },
+        "size": {
+            "area": 23088,
+            "x_range": [1089, 1141],
+            "y_range": [601, 1045]
+        }
+    }, {
+        "class": "rectangle",
+        "score": 0.9,
+        "label": [],
+        "color": {
+            "green": 1.0
+        },
+        "bbox": {
+            "x": 1144,
+            "y": 732,
+            "width": 53,
+            "height": 314
+            },
+        "mask": [[
+            [1145, 733],
+            [1145, 1045],
+            [1196, 1045],
+            [1196, 733]
+        ]],
+        "position": {
+            "x": 1170.5,
+            "y": 889
+        },
+        "size": {
+            "area": 15912,
+            "x_range": [1145, 1196],
+            "y_range": [733, 1045]
+        }
+    }
+]
+
+# Auxiliary Entities
+BAR_AUX = [
+    {
+        "class": "legend",
+        "score": 0.9,
+        "label": ["Samsung"],
+        "color": {
+            "blue": 1.0
+        },
+        "legend_data": {
+            "color": "blue"
+        },
+        "bbox": {
+            "x": 1241,
+            "y": 122,
+            "width": 166,
+            "height": 44
+            },
+        "mask": [[
+            [1242, 123],
+            [1242, 165],
+            [1406, 165],
+            [1406, 123]
+        ]],
+        "position": {
+            "x": 1324,
+            "y": 144
+        },
+        "size": {
+            "area": 6888,
+            "x_range": [1242, 1406],
+            "y_range": [123, 165]
+        }
+    }, {
+        "class": "legend",
+        "score": 0.9,
+        "label": ["Apple"],
+        "color": {
+            "orange": 1.0
+        },
+        "legend_data": {
+            "color": "orange"
+        },
+        "bbox": {
+            "x": 1241,
+            "y": 161,
+            "width": 124,
+            "height": 41
+            },
+        "mask": [[
+            [1242, 162],
+            [1242, 201],
+            [1364, 201],
+            [1364, 162]
+        ]],
+        "position": {
+            "x": 1303,
+            "y": 181.5
+        },
+        "size": {
+            "area": 4758,
+            "x_range": [1242, 1364],
+            "y_range": [162, 201]
+        }
+    }, {
+        "class": "legend",
+        "score": 0.9,
+        "label": ["Huawei"],
+        "color": {
+            "green": 1.0
+        },
+        "legend_data": {
+            "color": "green"
+        },
+        "bbox": {
+            "x": 1241,
+            "y": 197,
+            "width": 144,
+            "height": 42
+            },
+        "mask": [[
+            [1242, 198],
+            [1242, 238],
+            [1384, 238],
+            [1384, 198]
+        ]],
+        "position": {
+            "x": 1313,
+            "y": 218
+        },
+        "size": {
+            "area": 5680,
+            "x_range": [1242, 1384],
+            "y_range": [198, 238]
+        }
+    }, {
+        "class": "axis",
+        "score": 0.9,
+        "label": ["Sales (In Millions)"],
+        "axis_data": {
+            "direction": 90,
+            "ticks": [
+                {
+                    "text": "0",
+                    "position": {
+                        "x": 111,
+                        "y": 1043.5
+                    },
+                    "bbox": {
+                        "x": 101,
+                        "y": 1029,
+                        "width": 20,
+                        "height": 29
+                    }
+                }, {
+                    "text": "50",
+                    "position": {
+                        "x": 103.5,
+                        "y": 941
+                    },
+                    "bbox": {
+                        "x": 87,
+                        "y": 926,
+                        "width": 33,
+                        "height": 30
+                    }
+                }, {
+                    "text": "100",
+                    "position": {
+                        "x": 96.5,
+                        "y": 837.5
+                    },
+                    "bbox": {
+                        "x": 72,
+                        "y": 822,
+                        "width": 49,
+                        "height": 31
+                    }
+                }, {
+                    "text": "150",
+                    "position": {
+                        "x": 96,
+                        "y": 735.5
+                    },
+                    "bbox": {
+                        "x": 72,
+                        "y": 721,
+                        "width": 48,
+                        "height": 29
+                    }
+                }, {
+                    "text": "200",
+                    "position": {
+                        "x": 95.5,
+                        "y": 630
+                    },
+                    "bbox": {
+                        "x": 70,
+                        "y": 615,
+                        "width": 51,
+                        "height": 30
+                    }
+                }, {
+                    "text": "250",
+                    "position": {
+                        "x": 95.5,
+                        "y": 526.5
+                    },
+                    "bbox": {
+                        "x": 71,
+                        "y": 512,
+                        "width": 49,
+                        "height": 29
+                    }
+                }, {
+                    "text": "300",
+                    "position": {
+                        "x": 95,
+                        "y": 424.5
+                    },
+                    "bbox": {
+                        "x": 70,
+                        "y": 410,
+                        "width": 50,
+                        "height": 29
+                    }
+                }, {
+                    "text": "350",
+                    "position": {
+                        "x": 95.5,
+                        "y": 321
+                    },
+                    "bbox": {
+                        "x": 71,
+                        "y": 306,
+                        "width": 49,
+                        "height": 30
+                    }
+                }, {
+                    "text": "400",
+                    "position": {
+                        "x": 95,
+                        "y": 218.5
+                    },
+                    "bbox": {
+                        "x": 69,
+                        "y": 205,
+                        "width": 52,
+                        "height": 27
+                    }
+                }, {
+                    "text": "450",
+                    "position": {
+                        "x": 95,
+                        "y": 115
+                    },
+                    "bbox": {
+                        "x": 70,
+                        "y": 102,
+                        "width": 50,
+                        "height": 26
+                    }
+                },
+            ]
+        },
+        "color": {
+            "white": 0.8,
+            "black": 0.2
+        },
+        "bbox": {
+            "x": 10,
+            "y": 104,
+            "width": 134,
+            "height": 962
+            },
+        "mask": [[
+            [11, 105],
+            [11, 1065],
+            [143, 1065],
+            [143, 105]
+        ]],
+        "position": {
+            "x": 77,
+            "y": 585
+        },
+        "size": {
+            "area": 126720,
+            "x_range": [11, 143],
+            "y_range": [105, 1065]
+        }
+    }, {
+        "class": "axis",
+        "score": 0.9,
+        "label": ["Year"],
+        "axis_data": {
+            "direction": 0,
+            "ticks": [
+                {
+                    "text": "2013",
+                    "position": {
+                        "x": 262,
+                        "y": 1068
+                    },
+                    "bbox": {
+                        "x": 230,
+                        "y": 1055,
+                        "width": 64,
+                        "height": 26
+                    }
+                }, {
+                    "text": "2014",
+                    "position": {
+                        "x": 477.5,
+                        "y": 1068
+                    },
+                    "bbox": {
+                        "x": 445,
+                        "y": 1054,
+                        "width": 65,
+                        "height": 28
+                    }
+                }, {
+                    "text": "2015",
+                    "position": {
+                        "x": 690.5,
+                        "y": 1068
+                    },
+                    "bbox": {
+                        "x": 659,
+                        "y": 1055,
+                        "width": 63,
+                        "height": 26
+                    }
+                }, {
+                    "text": "2016",
+                    "position": {
+                        "x": 904,
+                        "y": 1068.5
+                    },
+                    "bbox": {
+                        "x": 872,
+                        "y": 1055,
+                        "width": 64,
+                        "height": 27
+                    }
+                }, {
+                    "text": "2017",
+                    "position": {
+                        "x": 1118,
+                        "y": 1068
+                    },
+                    "bbox": {
+                        "x": 1086,
+                        "y": 1055,
+                        "width": 64,
+                        "height": 26
+                    }
+                }
+            ]
+        },
+        "color": {
+            "white": 0.8,
+            "black": 0.2
+        },
+        "bbox": {
+            "x": 129,
+            "y": 1039,
+            "width": 1122,
+            "height": 89
+            },
+        "mask": [[
+            [130, 1040],
+            [130, 1127],
+            [1250, 1127],
+            [1250, 1040]
+        ]],
+        "position": {
+            "x": 690,
+            "y": 1083.5
+        },
+        "size": {
+            "area": 97440,
+            "x_range": [11, 143],
+            "y_range": [105, 1065]
+        }
+    }
+]
--- apis/annotation/libs/tonlp.py
+++ apis/annotation/libs/tonlp.py
@@ -4,8 +4,11 @@ class NLPData:
     """The class for data organization"""
     def __init__(self, data=None, auxiliary=None):
         self.data = data
+        self.auxiliary = auxiliary
         self.nlp_data = {
-            "labels": self.get_labels()
+            "labels": self.get_labels(),
+            "axes": self.get_axes(),
+            "legends": self.get_legends()
         }
 
     def get_labels(self):
@@ -19,6 +22,51 @@ class NLPData:
                         if label not in labels:
                             labels.append(label)
         return labels
+
+    def get_axes(self):
+        """The function for getting all the axes"""
+        axes = []
+        if self.auxiliary is not None:
+            for entity in self.auxiliary:
+                if entity.get("class") == "axis":
+                    # Get the label of the axis, including the title and the unit
+                    axis_label = entity.get("label")
+                    # Get the tick values of the axis
+                    axis_ticks = []
+                    axis_data = entity.get("axis_data")
+                    if axis_data is not None:
+                        ticks = axis_data.get("ticks")
+                        if ticks is not None:
+                            for tick in ticks:
+                                axis_ticks.append(tick["text"])
+                    # Pack the axis data
+                    axis = {
+                        "label": axis_label,
+                        "ticks": axis_ticks
+                    }
+                    axes.append(axis)
+        return axes
+
+    def get_legends(self):
+        """The function for getting all the legends"""
+        legends = []
+        if self.auxiliary is not None:
+            for entity in self.auxiliary:
+                if entity.get("class") == "legend":
+                    # Get the label of the legend, including the title and the unit
+                    legend_label = entity.get("label")
+                    # Get the feature of the legend
+                    legend_feature = {}
+                    legend_data = entity.get("legend_data")
+                    if legend_data is not None:
+                        legend_feature["color"] = legend_data.get("color")
+                    # Pack the legend data
+                    legend = {
+                        "label": legend_label,
+                        "feature": legend_feature
+                    }
+                    legends.append(legend)
+        return legends
 
     def get_result(self):
         """Return the organized data for the NLP module"""

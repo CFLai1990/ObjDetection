8f7620e55c636113c57a674def0eac6184e46966
CFLai1990 <music_lcf@163.com>
2019-03-08 16:58:16 +0800

    Testing shape detection. --ChufanLai

--- apis/annotation/libs/__settings__.py
+++ apis/annotation/libs/__settings__.py
@@ -4,8 +4,8 @@ import os
 #Detectron settings
 DT = {
     'config': '/home/chufan.lai/packages/Detectron/configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml',
-    'weights': '/home/chufan.lai/packages/detectron_models/demo/model_final.pkl',
-    # 'weights': '/tmp/detectron-output/train/coco_2014_train/generalized_rcnn/model_final.pkl',
+    # 'weights': '/home/chufan.lai/packages/detectron_models/demo/model_final.pkl',
+    'weights': '/tmp/detectron-output/train/coco_2014_train/generalized_rcnn/model_final.pkl',
     'threshold_detection': 0.7,
     'threshold_keypoint': 2.0,
 }

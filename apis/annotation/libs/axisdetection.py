c7540b77b43b860c8173c2bcb9e55eff8ae86ce9
CFLai1990 <music_lcf@163.com>
2019-03-16 19:40:14 +0800

    Testing axis detection. --ChufanLai

--- apis/annotation/libs/axisdetection.py
+++ apis/annotation/libs/axisdetection.py
@@ -45,7 +45,6 @@ def classify_texts(direction, f_items, ticks, labels):
         pos = f_item.get("position")
         proj_pos = [pos.get("x") * vertical_vector.get("x") + pos.get("y") * vertical_vector.get("y")]
         proj_positions.append(proj_pos)
-    print("projected positions: ", proj_positions)
     proj_data = np.array(proj_positions)
     # Classify based on the projected positions
     estimator = KMeans(n_clusters=2)
@@ -157,8 +156,13 @@ def get_axes_texts(img_path, axis_entities):
                 if axis_x and axis_y and axis_width and axis_height:
                     if axis_x >= 0 and axis_y >= 0 and axis_width > 0 and axis_height > 0:
                         print("axis range: ", axis_x, ", ", axis_y, ", ", axis_x + axis_width, ", ", axis_y + axis_height)
-                        axis_image = image.crop((axis_x, axis_y, axis_x + axis_width, axis_y + axis_height))
-                        axis_texts = pt.image_to_data(axis_image, lang=TS_LANG)
+                        axis_img = image.crop((axis_x, axis_y, axis_x + axis_width, axis_y + axis_height))
+                        axis_img_gray = np.asarray(image.convert("L"))
+                        line_sum = np.sum(axis_img_gray, axis=1)
+                        column_sum = np.sum(axis_img_gray, axis=0)
+                        print("line_sum: ", line_sum)
+                        print("column_sum: ", column_sum)
+                        axis_texts = pt.image_to_data(axis_img, lang=TS_LANG)
                         axis_texts = understand_data(axis_texts)
                         formated_axis = get_format_axis(axis_texts, axis_bbox, axis_direction)
                         data.append(formated_axis)

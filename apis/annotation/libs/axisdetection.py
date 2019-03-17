7f3f2ce3daad8a497c4f9d7778f7dd9dbe456791
CFLai1990 <music_lcf@163.com>
2019-03-17 17:48:15 +0800

    Testing axis detection. --ChufanLai

--- apis/annotation/libs/axisdetection.py
+++ apis/annotation/libs/axisdetection.py
@@ -162,19 +162,103 @@ def get_format_axis(items, axis_bbox, axis_direction):
     }
     return axis
 
-def partition_axis(axis_img_gray, axis_id):
+def divide_by_threshold(array, threshold, min_count=1):
+    """Find ranges with the value larger than the given threshold"""
+    # Step 1: divide the array by the given threshold
+    empty_ranges = {}
+    temp_range = {}
+    # Step 1-1: find the empty ranges
+    for _id, _value in enumerate(array):
+        if _value <= threshold:
+            if temp_range:
+                temp_range["end"] = _id
+                temp_range["length"] = temp_range["length"] + 1
+            else:
+                temp_range["start"] = _id
+                temp_range["end"] = _id
+                temp_range["length"] = 1
+        elif temp_range:
+            temp_range = {}
+        if temp_range and temp_range["length"] >= min_count:
+            range_head = temp_range["start"]
+            if empty_ranges.get(range_head) is None:
+                empty_ranges[range_head] = temp_range
+    # Step 1-2: find the non-empty ranges
+    solid_ranges = {}
+    temp_range = {}
+    for _id, _value in enumerate(array):
+        is_empty = False
+        for empty_range in empty_ranges:
+            if empty_range["start"] <= _id <= empty_range["end"]:
+                is_empty = True
+                break
+        if not is_empty:
+            if temp_range:
+                temp_range["end"] = _id
+                temp_range["length"] = temp_range["length"] + 1
+            else:
+                temp_range["start"] = _id
+                temp_range["end"] = _id
+                temp_range["length"] = 1
+        else:
+            temp_range = {}
+        if temp_range and temp_range["length"] > 1:
+            range_head = temp_range["start"]
+            if solid_ranges.get(range_head) is None:
+                solid_ranges[range_head] = temp_range
+    # Step 2: Decide which range belongs to which category
+    line_range = None
+    tick_range = None
+    title_range = None
+    range_values = list(solid_ranges.values())
+    range_count = len(range_values)
+    # Case 1: only the ticks
+    if range_count == 1:
+        tick_range = range_values[0]
+    # Case 2: line + tick or line + tick + title
+    elif range_count > 0:
+        min_range_len = float('Inf')
+        min_range_id = -1
+        # Assume the line to be the smallest range
+        for range_id, range_value in enumerate(range_values):
+            if range_value["length"] < min_range_len:
+                min_range_len = len(range_value)
+                min_range_id = range_id
+        if min_range_id >= 0:
+            line_range = range_values.pop(min_range_id)
+        # Find the range closest to the line
+        if line_range:
+            min_dist = float('Inf')
+            min_dist_id = -1
+            for range_id, range_value in enumerate(range_values):
+                dist = min(\
+                    abs(range_value["start"] - line_range["start"]), \
+                    abs(range_value["start"] - line_range["end"]), \
+                    abs(range_value["end"] - line_range["start"]), \
+                    abs(range_value["end"] - line_range["end"]))
+                if dist < min_dist:
+                    min_dist = dist
+                    min_dist_id = range_id
+            if min_dist_id >= 0:
+                tick_range = range_values.pop(min_dist_id)
+            # If there are still ranges left, it must be the title
+            if range_values:
+                title_range = range_values[0]
+    return line_range, tick_range, title_range
+
+def partition_axis(axis_img_gray, axis_id, axis_direction):
     """Partition the axis image into three parts: line, tick_texts and title"""
     line_img = None
-    tick_text_img = None
+    tick_img = None
     title_img = None
+    # Initialize
     axis_array = np.array(axis_img_gray, dtype=np.uint8)
     row_num = axis_array.shape[0]
     col_num = axis_array.shape[1]
     # Simplify the gray scales
-    axis_array = (axis_array / GRAY_SCALE_LEVEL).astype(np.uint8)
-    axis_array = axis_array * GRAY_SCALE_LEVEL
-    # row_avg = (np.sum(axis_array, axis=1) / axis_array.shape[1]).astype(np.uint8)
-    # col_avg = (np.sum(axis_array, axis=0) / axis_array.shape[0]).astype(np.uint8)
+    axis_array_simp = (axis_array / GRAY_SCALE_LEVEL).astype(np.uint8)
+    axis_array_simp = axis_array_simp * GRAY_SCALE_LEVEL
+    # Calculate the entropy of the simplified image
     row_ent = np.zeros(row_num)
     col_ent = np.zeros(col_num)
     for i in range(row_num):
@@ -183,11 +267,28 @@ def partition_axis(axis_img_gray, axis_id):
     for j in range(col_num):
         img_col = axis_array[:, j].tolist()
         col_ent[j] = entropy(img_col)
+    # Divide the image
+    if axis_direction == 0:
+        line_range, tick_range, title_range = divide_by_threshold(row_ent, 0, 3)
+        line_img = axis_img_gray.crop((0, line_range["start"], col_num - 1, line_range["end"]))
+        tick_img = axis_img_gray.crop((0, tick_range["start"], col_num - 1, tick_range["end"]))
+        title_img = axis_img_gray.crop((0, title_range["start"], col_num - 1, title_range["end"]))
+    elif axis_direction == 90:
+        line_range, tick_range, title_range = divide_by_threshold(col_ent, 0, 3)
+        line_img = axis_img_gray.crop((line_range["start"], 0, line_range["end"], row_num - 1))
+        tick_img = axis_img_gray.crop((tick_range["start"], 0, tick_range["end"], row_num - 1))
+        title_img = axis_img_gray.crop((title_range["start"], 0, title_range["end"], row_num - 1))
     np.savetxt('/home/chufan.lai/axis_' + str(axis_id) + '_row.txt', row_ent)
     np.savetxt('/home/chufan.lai/axis_' + str(axis_id) + '_col.txt', col_ent)
     test_img = Image.fromarray(axis_array)
     test_img.save('/home/chufan.lai/axis_' + str(axis_id) + '.png')
-    return line_img, tick_text_img, title_img
+    if line_img:
+        line_img.save('/home/chufan.lai/axis_' + str(axis_id) + '_line.png')
+    if tick_img:
+        tick_img.save('/home/chufan.lai/axis_' + str(axis_id) + '_tick.png')
+    if title_img:
+        title_img.save('/home/chufan.lai/axis_' + str(axis_id) + '_title.png')
+    return line_img, tick_img, title_img
 
 def contrast_enhance(axis_img_PIL):
     """Enhance the contrast of the axis image"""
@@ -227,7 +328,7 @@ def get_axes_texts(img_path, axis_entities):
                         # Step 2: enhance the contrast
                         axis_img_gray = contrast_enhance(axis_img).convert("L")
                         # Step 3: partition the image
-                        line_img, tick_text_img, title_img = partition_axis(axis_img_gray, axis_id)
+                        line_img, tick_img, title_img = partition_axis(axis_img_gray, axis_id, axis_direction)
                         print("partition finished")
                         axis_texts = pt.image_to_data(axis_img_gray, lang=TS_LANG)
                         print("ocr finished")

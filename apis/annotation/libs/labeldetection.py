36ba57fb6f0a718aef8b7ccf3ea44482f77b1ad8
CFLai1990 <music_lcf@163.com>
2019-03-21 00:11:50 +0800

    Testing label detection. --ChufanLai

--- apis/annotation/libs/labeldetection.py
+++ apis/annotation/libs/labeldetection.py
@@ -7,6 +7,8 @@ from sklearn.cluster import KMeans
 from .__settings__ import TESTING
 from .image_processing import CV2PIL, contrast_enhance
 
+COLOR_RANGE = 16
+
 def get_major_color(colors, colors_rgb):
     """Get the major color of the entity"""
     max_score = float('-inf')
@@ -64,13 +66,19 @@ def get_label_texts(img, data_entities):
                             data_x:(data_x + data_width)].copy()
                         data_mask = mask_img[data_y:(data_y + data_height),\
                             data_x:(data_x + data_width)]
-                        # Step 1: fill the other areas with the major color
                         if TESTING["label"]["sign"]:
                             cv2.imwrite(TESTING['dir'] + '/label_' + str(data_id) + '_mask.png', \
                                 data_mask, \
                                 [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+                        # Step 1: fill the other areas with the major color
                         data_img[np.where(data_mask == 0)] = major_color_bgr
-                        data_img_enhanced = contrast_enhance(data_img)
+                        # Step 2: smooth the similar colors
+                        major_color_upper = np.array(major_color_bgr, dtype=np.int8) + COLOR_RANGE
+                        major_color_lower = np.array(major_color_bgr, dtype=np.int8) - COLOR_RANGE
+                        data_img[np.where((data_mask > 0) \
+                            & ((data_mask < major_color_upper).all()) \
+                            & ((data_mask > major_color_lower).all()))] = major_color_bgr
+                        # data_img_enhanced = contrast_enhance(data_img)
                         (data_img_h, data_img_w) = data_img.shape[:2]
                         data_img_enhanced = cv2.cvtColor(data_img_enhanced, \
                             cv2.COLOR_BGR2GRAY).astype(np.uint8)

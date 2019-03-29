7caefb700128d367d17af7e785866c5247b0b9c9
CFLai1990 <music_lcf@163.com>
2019-03-29 20:34:21 +0800

    Testing new model. --ChufanLai

--- apis/annotation/libs/vis_dataset.py
+++ apis/annotation/libs/vis_dataset.py
@@ -5,9 +5,9 @@ def get_vis_dict():
     """The dataset that includes all the 'classes' field."""
     ds_dict = AttrDict()
     classes = [
-        '__ignore__', '_background_', 'rectangle', 'axis', 'legend', 'pie'
+        '__ignore__', '_background_', 'rectangle', 'axis', 'legend', 'sector', 'circle'
     ]
     ds_dict.classes = {i: name for i, name in enumerate(classes)}
     return ds_dict
 
-FIX_DICT = ['rectangle', 'pie']
+FIX_DICT = ['rectangle', 'sector']
